452bfcc6b62f7a7173cbb7aafc2f6462b4f5bea2
Geert Dierick <geert2dierick@gmail.com>
2026-01-16 12:21:43 +0100

    Client_Add fetch-data and nav-menu

--- healthcheck.client/src/app/app.module.ts
+++ healthcheck.client/src/app/app.module.ts
@@ -3,13 +3,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FetchDataComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
